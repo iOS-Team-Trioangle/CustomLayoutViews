8811ba2ab04a32dc3cea5502509d77adbec5a2c1
Karuppasamy Palanikumar <ios@trioangle.com>
2022-02-15 10:10:05 +0530

    Project Library Added -> Karuppasamy

---
+++ CustomLayoutViews/CustomLayoutViews.docc/Resources/SafeAreaLayoutView.swift
@@ -0,0 +1,15 @@
+//
+//  SafeAreaLayoutView.swift
+//  CustomLayoutViews
+//
+//  Created by Trioangle on 15/02/22.
+//
+
+import Foundation
+import UIKit
+
+class SafeAreaLayoutView: UIView {
+    class func initView() {
+        
+    }
+}
---
+++ CustomLayoutViews/CustomLayoutViews.h
@@ -0,0 +1,18 @@
+//
+//  CustomLayoutViews.h
+//  CustomLayoutViews
+//
+//  Created by Trioangle on 15/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomLayoutViews.
+FOUNDATION_EXPORT double CustomLayoutViewsVersionNumber;
+
+//! Project version string for CustomLayoutViews.
+FOUNDATION_EXPORT const unsigned char CustomLayoutViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomLayoutViews/PublicHeader.h>
+
+
